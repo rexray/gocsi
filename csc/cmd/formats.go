dc38588b1b3af432dc5c9492653098009d8d9396
Sunny <me@darkowlzz.space>
2018-04-09 13:59:42 +0530

    csc: Rename VolumeInfo to Volume in listVolumesFormat
    This change is required because CSI spec renamed `VolumeInfo` to `Volume`
    and `csc controller list-volumes` fails with template execution error.
    
    Signed-off-by: Sunny <me@darkowlzz.space>

--- csc/cmd/formats.go
+++ csc/cmd/formats.go
@@ -10,7 +10,7 @@ const volumeInfoFormat = `{{printf "%q\t%d" .Id .CapacityBytes}}` +
 // listVolumesFormat is the default Go template format for emitting a
 // ListVolumesResponse
 const listVolumesFormat = `{{range $k, $v := .Entries}}` +
-	`{{with $v.VolumeInfo}}` + volumeInfoFormat + `{{end}}` +
+	`{{with $v.Volume}}` + volumeInfoFormat + `{{end}}` +
 	`{{end}}` + // {{range $v .Entries}}
 	`{{if .NextToken}}{{printf "token=%q\n" .NextToken}}{{end}}`
 
